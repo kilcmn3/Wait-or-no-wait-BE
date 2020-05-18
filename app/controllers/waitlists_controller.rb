cb6f28be8aafda1262457af45f34b3624cb3f5b1
kilcmn <kilcmn3@hotmail.com>
2020-05-18 11:39:16 -0400

    waitlist controller create done

--- app/controllers/waitlists_controller.rb
+++ app/controllers/waitlists_controller.rb
@@ -1,6 +1,18 @@
 class WaitlistsController < ApplicationController
     def index
-        wait_List =  Waitlist.first
+        # byebug
+        wait_List =  Waitlist.last
         render json: wait_List, include: 'customers'
+    end
+
+    def create
+        wait_List = Waitlist.create(params_waitList)
+        render json: wait_List
+    end
+
+    private
+
+    def params_waitList
+        params.require(:waitlist).permit(:waitlist_date, :owner_id)
     end
 end
