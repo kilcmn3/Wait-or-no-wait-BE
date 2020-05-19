c7889163aafad14314ed7cfb73ccfbcf23d71497
kilcmn <kilcmn3@hotmail.com>
2020-05-19 06:44:10 -0400

    feature/cust_waitlist

--- app/controllers/waitlists_controller.rb
+++ app/controllers/waitlists_controller.rb
@@ -1,19 +1,24 @@
 class WaitlistsController < ApplicationController
     def index
         wait_List =  Waitlist.all
+        if !Waitlist.find_by(waitlist_date: Date.today.to_s)
+            owner = Owner.last
+            wait_List = Waitlist.new(waitlist_date: Date.today.to_s, owner_id: owner.id)
+            wait_List.save
+        end
         render json: wait_List, include: 'customers'
     end
 
     ##TODO Owner params
-    def create
-        owner = Owner.last
-        wait_list= Waitlist.find_by(owner_id: owner.id)
-        if !wait_list
-            wait_list = Waitlist.new(waitlist_date: params["waitlist"],  owner_id: owner.id)
-            wait_list.save
-        end
-        render json: wait_list
-    end
+    # def create
+    #     owner = Owner.last
+    #     wait_list= Waitlist.find_by(owner_id: owner.id)
+    #     if !wait_list
+    #         wait_list = Waitlist.new(waitlist_date: params["waitlist"],  owner_id: owner.id)
+    #         wait_list.save
+    #     end
+    #     render json: wait_list
+    # end
 
     # private
 
