824084ee41722621259474926585c39934e66833
kilcmn <kilcmn3@hotmail.com>
2020-05-20 01:18:54 -0400

    succed on redux+react  hook

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -10,9 +10,17 @@ class CustomersController < ApplicationController
     def create
         customer = Customer.create(params_customer)
         wait_List =  Waitlist.find_by(waitlist_date: Date.today.to_s)
-        totalTime = Customer.all.length * 3 
+
+        if wait_List.customers.all.length == 0
+            count = 3
+        elsif wait_List.customers.all.length == 1
+            count = 6
+        else
+            count = wait_List.customers.all.length * 3 
+        end
+
         cust_waitlist = CustomerWaitlist.new( 
-            estimate_waitTime: totalTime == 0 ? 3 : totalTime, 
+            estimate_waitTime: count, 
             check_inTime: Time.now().to_s, 
             party_size: params["wait_list"]["party_size"], 
             waitlist_id: wait_List.id, 
