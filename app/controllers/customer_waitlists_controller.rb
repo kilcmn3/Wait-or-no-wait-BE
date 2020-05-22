06cfa34b519ba94948220c0a4d2c78440b6ce3bc
kilcmn <kilcmn3@hotmail.com>
2020-05-22 13:05:37 -0400

    update is_waiting

--- app/controllers/customer_waitlists_controller.rb
+++ app/controllers/customer_waitlists_controller.rb
@@ -1,13 +1,17 @@
 class CustomerWaitlistsController < ApplicationController
     def update
-        customer_waitlist = CustomerWaitlist.find_by(customer_id: params[:id])
-        customer_waitlist.update(paramse_update)
+        customer_waitlist = CustomerWaitlist.find(params[:id])
+        if  params[:customer_waitlist][:estimate_waitTime] && params[:customer_waitlist][:estimate_waitTime] < 0
+            customer_waitlist.update(estimate_waitTime: 0)
+        else
+            customer_waitlist.update(paramse_update)
+        end
         render json: customer_waitlist
-    end
-
+end
+    
     private
-
+    
     def  paramse_update
-        params.require(:customer_waitlist).permit(:is_waiting, :estimate_waitTime)
+        params.require(:customer_waitlist).permit(:is_waiting, :estimate_waitTime, :is_texted)
     end
 end
