5aedc233b2e18308db12ced8047ba3f31364699f
kilcmn <kilcmn3@hotmail.com>
2020-05-24 04:36:55 -0400

    working on post

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,2 +1,17 @@
 class OwnersController < ApplicationController
+    def login 
+        owner = Owner.find_by(email: params[:owner][:email])
+        render json: owner
+    end
+
+    def signup
+        owner = Owner.create(params_owner)
+        render json: owner
+    end
+
+    private
+
+    def  params_owner
+        params.require(:owner).permit(:username, :password, :restaurant_name,:restaurant_location,:restaurant_contact)
+    end
 end
